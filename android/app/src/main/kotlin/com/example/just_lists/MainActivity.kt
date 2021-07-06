6e69f0afa57b7fe442796c8a89e92716f84a3ff2
jgdml <47838646+jgdml@users.noreply.github.com>
2021-07-05 22:50:28 -0300

    Base

---
+++ android/app/src/main/kotlin/com/example/just_lists/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.just_lists
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:just_lists/screens/home.dart';
+
+
+void main() {
+    runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+
+@override
+Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+        ),
+        home: Home(),
+        );
+    }   
+}
+
