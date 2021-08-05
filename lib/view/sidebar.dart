8bdb4c1b7e5c8737eaac779a8e1b5d8fa71b7d7b
jgdml <47838646+jgdml@users.noreply.github.com>
2021-08-04 23:41:53 -0300

    Sidebar inicial

--- lib/view/sidebar.dart
+++ lib/view/sidebar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:just_lists/view/components/list_button.dart';
 
 class Sidebar extends StatelessWidget {
     const Sidebar({Key? key}) : super(key: key);
@@ -14,12 +16,45 @@ class Sidebar extends StatelessWidget {
             child: Column(
                 children: <Widget>[
 
+                    SizedBox(height: screenH*0.03),
+
                     Image(
                         image: AssetImage('assets/logo.png'),
                         width: screenW*0.1,
                     ),
 
                     SizedBox(height: screenH*0.03),
+
+                    ListButton(
+                        texto: Text("Nova lista"), 
+                        icone: Icon(Icons.add), 
+                        func: () => null
+                    ),
+
+                    Container(
+                        width: 200,
+                        height: screenH*0.7,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            physics: AlwaysScrollableScrollPhysics(),
+
+                            itemCount: 50,
+                            itemBuilder: (BuildContext context, int index){
+                                return ListButton(
+                                    texto: Text("Lista "+(index+1).toString()), 
+                                    icone: Icon(Icons.list), 
+                                    func: () => print(index.toString())
+                                );
+                            }
+                        ),
+                    ),
+                
+                    ListButton(
+                        texto: Text("Configurações"), 
+                        icone: Icon(Icons.settings), 
+                        func: () => null
+                    ),
                 ],
             ),
         );
