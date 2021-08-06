14d8240c4406a468696e2b1f563b295ea6a7f613
jgdml <47838646+jgdml@users.noreply.github.com>
2021-08-06 11:38:34 -0300

    nome da pasta component => widget

--- lib/view/widget/list_button.dart
+++ lib/view/widget/list_button.dart
@@ -16,13 +16,12 @@ class ListButton extends StatelessWidget {
     Widget build(BuildContext context) {
 
         return Container(
-            margin: EdgeInsets.only(bottom: 15),
             child: TextButton.icon(
                 onPressed: func,
                 icon: this.icone,
                 label: this.texto,
                 style: ButtonStyle(
-                    padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(20))
+                    padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(20)),
                 ),
             )
         );
