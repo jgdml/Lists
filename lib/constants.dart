d2997a0ffe1f23e1bd308e7a5861141840b850cf
jgdml <47838646+jgdml@users.noreply.github.com>
2021-08-12 18:27:27 -0300

    Tipo de lista

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,9 @@ class Constants{
     static const NAV_MY_LISTS = '/myLists';
     static const NAV_CREATE_LIST = '/createList';
     static const NAV_CONFIG = '/config';
+
+    static const TYPE_STR = 1;
+    static const TYPE_INT = 2;
+    static const TYPE_BOOL = 3;
+    static const TYPE_DOUBLE = 4;
 }
--- lib/domain/model/lista.dart
+++ lib/domain/model/lista.dart
@@ -1,10 +1,12 @@
+import 'package:just_lists/domain/model/tipo_lista.dart';
+
 class Lista{
 
     int? id;
     
     String nome;
-    List<String> campos;
+    TipoLista tipoLista;
     bool isPublic = false;
 
-    Lista({this.id, required this.nome, required this.campos});
+    Lista({this.id, required this.nome, required this.tipoLista});
 }
