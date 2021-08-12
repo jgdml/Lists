170393e6cd53dcd0fc81f1d3ec408475d54339c2
jgdml <47838646+jgdml@users.noreply.github.com>
2021-08-12 20:02:06 -0300

    Titulo minhas listas

--- lib/view/my_lists.dart
+++ lib/view/my_lists.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:just_lists/view/widget/page_title.dart';
 
 class MyLists extends StatelessWidget {
     const MyLists({ Key? key }) : super(key: key);
 
     @override
     Widget build(BuildContext context) {
-        return Container(
-        
+        return Scaffold(
+            body: Center(
+                child: Column(
+                    children: [
+                        Spacer(),
+
+                        PageTitle(
+                            texto: "Minhas Listas",
+                        ),
+
+                        Spacer(flex: 5),
+
+                        Spacer(flex: 8)
+                    ],
+                ),
+            ),
         );
     }
 }
