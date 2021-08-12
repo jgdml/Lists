d394c035e29402e151a156c65f0b7055c3a04e2e
jgdml <47838646+jgdml@users.noreply.github.com>
2021-08-12 20:11:27 -0300

    botao em bloco

---
+++ lib/view/widget/block_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BlockButton extends StatelessWidget {
+
+    final IconData icone;
+    final String titulo;
+    final String subtitulo;
+    final Function()? func;
+
+    const BlockButton({ 
+        Key? key, 
+        required this.icone, 
+        required this.titulo, 
+        required this.subtitulo, 
+        this.func
+    }) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 3, offset: Offset(2, 2))]
+            ),
+            padding: EdgeInsets.all(50),
+
+            child: InkWell(
+                onTap: () => null,
+                child: Column(children: [
+                    Icon(icone, size: 50, color: Theme.of(context).primaryColor),
+                    Text(
+                        titulo, 
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold
+                        )
+                    ),
+                    Text(
+                        subtitulo,
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.grey.shade600
+                        ),
+                    )
+                ]),
+            ),
+        );
+    }
+}
