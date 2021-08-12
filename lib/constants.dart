08b94b37280120dcbb523bdf01f5f1ed2f9bbf6b
jgdml <47838646+jgdml@users.noreply.github.com>
2021-08-12 13:48:16 -0300

    Nova rota

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 class Constants{
     static const NAV_HOME = '/';
+    static const NAV_MY_LISTS = '/myLists';
     static const NAV_CREATE_LIST = '/createList';
     static const NAV_CONFIG = '/config';
 }
