3487a729b29649a8ef5ed24668199402c482ef08
jgdml <47838646+jgdml@users.noreply.github.com>
2021-08-19 23:24:49 -0300

    telas de criacao e listagem

--- lib/view/create_list.dart
+++ lib/view/create_list.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:just_lists/view/widget/custom_button.dart';
 import 'package:just_lists/view/widget/decorated_container.dart';
 import 'package:just_lists/view/widget/page_title.dart';
 import 'package:just_lists/view/widget/responsive_text.dart';
 
 class CreateList extends StatefulWidget {
-    const CreateList({ Key? key }) : super(key: key);
+    const CreateList({Key? key}) : super(key: key);
 
     @override
     _CreateListState createState() => _CreateListState();
@@ -13,23 +14,22 @@ class CreateList extends StatefulWidget {
 class _CreateListState extends State<CreateList> {
     final _formState = GlobalKey<FormState>();
 
-
     bool isPrivate = true;
 
-    Widget titleField(){
+    Widget titleField() {
         return TextFormField(
             maxLength: 50,
-            
+            autofocus: true,
             style: TextStyle(fontSize: 18),
             decoration: InputDecoration(
                 labelText: "Título",
                 hintText: "Lista número 1",
-                border: OutlineInputBorder()
+                border: OutlineInputBorder(),
             ),
         );
     }
 
-    Widget privacyField(){
+    Widget privacyField() {
         return FittedBox(
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -44,63 +44,114 @@ class _CreateListState extends State<CreateList> {
                         value: isPrivate,
                     ),
                     isPrivate ? Icon(Icons.lock_outline) : Icon(Icons.lock_open),
-                    Text("Lista " + (isPrivate ? "privada" : "pública"), style: TextStyle(fontSize: 16))
+                    Text(
+                        "Lista " + (isPrivate ? "privada" : "pública"),
+                        style: TextStyle(fontSize: 16),
+                    ),
                 ],
             ),
         );
     }
 
-    Widget typeField(){
+    Widget campoField() {
+        var tipos = ['Texto', 'Número', 'Preço', 'Checkmark'];
+
         return FittedBox(
-          child: DropdownButton(
-              items: ['tipo 1 ', 'tipo2'].map((var val) {
-                  return DropdownMenuItem(value: val, child: Text(val));
-              }).toList(),
-              onChanged: (a) => null,
-              hint: Text('Selecione o tipo da lista'),
-              style: TextStyle(fontSize: 18),
-          ),
+            child: Container(
+                width: 700,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                margin: EdgeInsets.symmetric(vertical: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                        width: 5,
+                        color: Theme.of(context).primaryColor.withAlpha(20),
+                    ),
+                ),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                        Container(
+                            width: 300,
+                            child: TextFormField(
+                                style: TextStyle(fontSize: 18),
+                                decoration: InputDecoration(
+                                    labelText: "Campo",
+                                    hintText: "Campo número 1",
+                                    border: OutlineInputBorder(),
+                                ),
+                            ),
+                        ),
+                        DropdownButton(
+                            items: tipos.map((var val) {
+                                return DropdownMenuItem(value: val, child: Text(val));
+                            }).toList(),
+                            onChanged: (a) => null,
+                            hint: Text('Selecione o tipo da lista'),
+                            style: TextStyle(fontSize: 18),
+                        ),
+                    ],
+                ),
+            ),
+        );
+    }
+
+    Widget addCampo() {
+        return CustomButton(
+            onPressed: () => null,
+            icon: Icon(Icons.add),
+            label: "Adicionar um campo",
+        );
+    }
+
+    Widget campos() {
+        return Column(
+            children: [
+                campoField(),
+                addCampo(),
+            ],
+        );
+    }
+
+    Widget saveButton() {
+        return CustomButton(
+            onPressed: () => null,
+            icon: Icon(Icons.save),
+            label: "Salvar",
         );
     }
 
     @override
     Widget build(BuildContext context) {
-
         return Scaffold(
             body: Center(
                 child: Column(
                     children: [
-
                         Spacer(),
-
                         PageTitle(texto: "Criar Lista"),
-
                         Spacer(flex: 5),
-
-                        DecoratedContainer(                        
+                        DecoratedContainer(
                             child: Form(
                                 key: _formState,
                                 child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                                     children: [
                                         titleField(),
-
+                                        campos(),
                                         Spacer(),
-
                                         privacyField(),
-
-                                        Spacer(),
-
-                                        typeField(),
-
-                                        Spacer(flex: 2)
+                                        Spacer(
+                                            flex: 2,
+                                        ),
+                                        saveButton()
                                     ],
                                 ),
                             ),
                         ),
-
                         Spacer(flex: 8),
-                    ],  
-                )
+                    ],
+                ),
             ),
         );
     }
--- lib/view/my_lists.dart
+++ lib/view/my_lists.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:just_lists/constants.dart';
+import 'package:just_lists/domain/model/campo.dart';
+import 'package:just_lists/domain/model/lista.dart';
+import 'package:just_lists/view/widget/block_button.dart';
 import 'package:just_lists/view/widget/page_title.dart';
 
 class MyLists extends StatelessWidget {
     const MyLists({ Key? key }) : super(key: key);
+
+    List<Lista> buscarListas(){
+        var lista = <Lista>[];
+
+        for (int i = 0; i < 5; i++){
+            lista.add(
+                Lista(
+                    nome: (i+1).toString(), 
+                    campos: [
+                        Campo(
+                            nome: 'campo'+i.toString(), 
+                            tipo: Constants.TYPE_INT
+                        )
+                    ]
+                )
+            );
+        }
+        return lista;
+    }
+
+    List<Widget> showLists(){
+        var res = buscarListas();
+
+        return res.map((lista) {
+            return BlockButton(
+                icone: Icons.list, 
+                titulo: lista.nome, 
+                subtitulo: "username",
+                func: () => null,
+            );
+        }).toList();
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -17,6 +53,14 @@ class MyLists extends StatelessWidget {
                         ),
 
                         Spacer(flex: 5),
+                        
+                        Wrap(
+                            alignment: WrapAlignment.spaceEvenly,
+                            spacing: 50,
+                            runSpacing: 50,
+                            
+                            children: showLists(),
+                        ),
 
                         Spacer(flex: 8)
                     ],
