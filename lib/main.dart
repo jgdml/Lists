2c5652dfcd78035424ba8ccbb737959f440e7a3b
jgdml <47838646+jgdml@users.noreply.github.com>
2021-08-24 17:21:25 -0300

    loading \ url #

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,34 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:just_lists/constants.dart';
 import 'package:just_lists/view/config.dart';
 import 'package:just_lists/view/create_list.dart';
 import 'package:just_lists/view/list_viewer.dart';
 import 'package:just_lists/view/lists.dart';
 import 'package:just_lists/view/widget/base.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 
 void main() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp();
-
+    
+    setPathUrlStrategy();
     runApp(JustLists());
+
 
 }
 
 class JustLists extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
+        EasyLoading.instance.indicatorType = EasyLoadingIndicatorType.ring;
+
         return MaterialApp(
             title: 'JustLists',
             theme: ThemeData(
-                primarySwatch: Colors.red,
+                primarySwatch: Colors.lightBlue,
             ),
             routes: {
                 Constants.NAV_HOME: (context) => Base(Lists(isHome: true)),
@@ -31,6 +37,8 @@ class JustLists extends StatelessWidget {
                 Constants.NAV_LIST_VIEWER: (context) => Base(ListViewer()),
                 Constants.NAV_CONFIG: (context) => Base(Config(), selected: 3)
             },
+
+            builder: EasyLoading.init(),
         );
     }
 }
